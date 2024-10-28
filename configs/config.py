40441c8ef82fe76c822407af0ca002204ad587cd
SuzukiHana <ikarugaruka111@gmail.com>
2024-10-28 23:19:23 +0800

    FEAT: implement config

--- configs/config.py
+++ configs/config.py
@@ -1,8 +1,70 @@
-class AssertConfig:
-    pass
+from typing import Optional, Any
 
-class SimulatorConfig:
-    pass
+import yaml
+
+from ..utils.logging import get_logger
 
 class Config:
-    pass
+    class AssertConfig:
+        def __init__(self) -> None:
+            self.common_assert: Optional[str] = None
+            self.LR_assert: Optional[str] = None
+            self.resource_record_size: Optional[list[int]] = None
+            self.max_io_buffer: Optional[int] = None
+            self.resource_list: list[str] = list()
+        
+        def load_from_yaml(self, yaml_path: str) -> None:
+            with open(yaml_path, 'r') as f:
+                cfg_dict = yaml.safe_load(f)
+            self.load_from_dict(cfg_dict)
+
+        def load_from_dict(self, assert_dict: dict) -> None:
+            self.common_assert = assert_dict['common_assert']
+            self.LR_assert = assert_dict['LR_assert']
+            self.resource_record_size = assert_dict['resource_record_size']
+            self.max_io_buffer = assert_dict['max_io_buffer']
+            if 'resource' in assert_dict.keys():
+                for resource_key in assert_dict['resource'].keys():
+                    setattr(self, resource_key, assert_dict['resource'][resource_key])
+                    self.resource_list.append(resource_key)
+
+    class SimulatorConfig:
+        def __init__(self) -> None:
+            self.adb_path: Optional[str] = None
+            self.device_addr: Optional[str] = None
+            self.screen_resolution: Optional[tuple[int, int]] = None
+            self.app_rotation: Optional[int] = None
+
+        def load_from_yaml(self, yaml_path: str) -> None:
+            with open(yaml_path, 'r') as f:
+                cfg_dict = yaml.safe_load(f)
+            self.load_from_dict(cfg_dict)
+
+        def load_from_dict(self, simulator_dict: dict) -> None:
+            self.adb_path = simulator_dict['adb_path']
+            self.device_addr = simulator_dict['device_addr']
+            self.screen_resolution = simulator_dict['screen_resolution']
+            self.app_rotation = simulator_dict['app_rotation']
+    
+    def __init__(self) -> None:
+        self.name: Optional[str] = None
+        self.assert_cfg: Config.AssertConfig = Config.AssertConfig()
+        self.simulator_cfg: Config.SimulatorConfig = Config.SimulatorConfig()
+        self.logger = None
+
+    @staticmethod
+    def load_from_yaml(yaml_path: str) -> 'Config':
+        config = Config()
+        with open(yaml_path, 'r') as f:
+            cfg_dict = yaml.safe_load(f)
+        config.assert_cfg.load_from_yaml(cfg_dict['assert_cfg_file'])
+        config.simulator_cfg.load_from_yaml(cfg_dict['simulator_cfg_file'])
+        config.name = cfg_dict['name']
+        config.logger = get_logger('framework.config.{config.name}')
+        config.logger.debug(f"load config from {yaml_path}")
+        return config
+    
+    def register_to(self, obj: Any) -> Any:
+        setattr(obj,'assert_cfg',self.assert_cfg)
+        setattr(obj,'simulator_cfg',self.simulator_cfg)
+        return self
--- configs/config_parser.py
+++ configs/config_parser.py
@@ -1,2 +1,38 @@
+import yaml
+import os
+
+from .config import Config
+from ..utils.logging import get_logger
+
 class ConfigParser:
-    pass
+    def __init__(self) -> None:
+        self.config_path_list: list[str] = list()
+        self.logger = get_logger("framework.config_parser")
+
+    def add_config_from_yaml(self, yaml_path: str) -> 'ConfigParser':
+        self.config_path_list.append(yaml_path)
+        return self
+
+    def generate_config(self,idx: int = 0) -> Config:
+        assert idx < len(self.config_path_list) and idx >= 0, f"index {idx} is out of range"
+        return Config.load_from_yaml(self.config_path_list[idx])
+    
+    @property
+    def config_path_list(self) -> list[str]:
+        return self.config_path_list
+
+    def __len__(self) -> int:
+        return len(self.config_path_list)
+
+    def search_from_dir(self, dir_name: str = './configs') -> Config:
+        for file in os.listdir(dir_name):
+            if file.endswith('.yaml'):
+                self.add_config_from_yaml(os.path.join(dir_name, file))
+        self.logger.debug(f"search configs from {dir_name}, find {len(self)} configs")
+        return self
+    
+    def __str__(self) -> str:
+        return f"ConfigParser with {len(self)} configs [{self.config_path_list}]"
+    
+    def __repr__(self) -> str:
+        return self.__str__()
