8247e39fe94fb2a7cc1ea0c304783d671ff57438
SuzukiHana <ikarugaruka111@gmail.com>
2024-10-28 23:20:01 +0800

    FEAT: modify import method of logging

---
+++ utils/logging/__init__.py
@@ -0,0 +1,9 @@
+from .get_logger import get_logger
+from .process_logger import ProgressLogger
+from .project_formatter import ProjectFormatter
+
+__all__ = [
+    "get_logger",
+    "ProgressLogger",
+    "ProjectFormatter",
+]
